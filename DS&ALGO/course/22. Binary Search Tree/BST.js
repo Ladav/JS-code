01752c73d21fb0744be1f188e34320c261c672c0
Davinder Singh <davindersaini003@gmail.com>
2020-02-17 23:31:10 +0530

    new

--- DS&ALGO/course/22. Binary Search Tree/BST.js
+++ DS&ALGO/course/22. Binary Search Tree/BST.js
@@ -20,21 +20,37 @@ class BST {
         } 
 
 
-        let tempNode = this.root;
+        let curNode = this.root;
         while (true) {
-            if (tempNode.val > newNode.val) {
-                if(tempNode.left === null) {
-                    tempNode.left = newNode;
+            if(newNode.val === curNode.val) return undefined; // node with same value exists already
+            if (curNode.val > newNode.val) {
+                if(curNode.left === null) {
+                    curNode.left = newNode;
                     return this;
                 }
-                tempNode = tempNode.left;
+                curNode = curNode.left;
             }
             else {
-                if(tempNode.right === null) {
-                    tempNode.right = newNode;
+                if(curNode.right === null) {
+                    curNode.right = newNode;
                     return this;
                 }
-                tempNode = tempNode.right;
+                curNode = curNode.right;
+            }
+        }
+    }
+    search(val) {
+        if(!this.root) return false;
+        
+        let curNode = this.root;
+        while(true) {
+            if(!curNode) return undefined;
+            if(curNode.val === val) return curNode;
+            if(curNode.val > val) {
+                curNode = curNode.left;
+            }
+            else {
+                curNode = curNode.right;
             }
         }
     }
@@ -43,4 +59,4 @@ class BST {
 const binaryTree = new BST();
 console.log(binaryTree.insert(1));
 console.log(binaryTree.insert(2));
-console.log(binaryTree.insert(0));
+console.log(binaryTree.search(0));
