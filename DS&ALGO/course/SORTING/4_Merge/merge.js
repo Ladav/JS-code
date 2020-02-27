18fff5148584dce4a8880f79ef54bcea9b711b99
Davinder Singh <davindersaini003@gmail.com>
2020-02-27 23:34:52 +0530

    new

--- DS&ALGO/course/SORTING/4_Merge/merge.js
+++ DS&ALGO/course/SORTING/4_Merge/merge.js
@@ -1,26 +1,38 @@
-const mergeSort = (arr) => {
-    // divide the array in two parts util only 1 element left's in it
-    let len = arr.length;
-    let mid;
-    let right = [], left = [];
-    if (len > 1) {
-        mid = (len / 2)|0;
-        left = arr.slice(0, mid);
-        left = mergeSort(left);
-        right = arr.slice(mid, len);
-        right = mergeSort(right);
-        return merge(left, right);
-    }
-    return arr;
-}
-/**
- *  [3,2,1]-> len=3, mid=1, left=[3], right=[2,1]
- *      /                                  \
+/** Divide Array into two parts util only one element left in the array recursively
+ *  then two the two divide arrays from bottom toward's top.
+ * 
+ *              [3,2,1]-> len=3, mid=1, left=[3], right=[2,1]
+ *                      /                            \
  *  [3]-> length=1 return [3]            [2,1]-> length=2 mid=1
  *                                          /               \
  *                                 [2] return [2]           [1] returns [1]
+ * 
+ *  merging of the above example will be like->
+ *  [3,2,1]-> divide into [3],[2,1]
+ * now [3] is sorted and can be merged with other part ex-[2,1]
+ * before ""merge(left, right);"" 
+ * we have to do ""right = mergeSort(arr.slice(mid, arr.length));""
+ * but [2,1] is not sorted till now so will be sorted as [2],[1]
+ * now we have [3,2,1]
+ *              /   \
+ *            [3]   [2,1]
+ *                  /   \
+ *                [2]   [1]
+ * step 1) merge [3] with the array in the right; [3] and [2,1]
+ * step 2) sort and merge [2,1] -> merge([2],[1]) -> [1,2]
+ * step 3) merge([3],[1,2]) -> [1,2,3]
  */
+const mergeSort = (arr) => {
+    if (arr.length === 1) return arr;
+    let mid = (arr.length / 2) | 0;
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid, arr.length));
+    return merge(left, right);
+}
 
+/**Merge two sorted arrays
+ * ([1,5,7], [2,4,9]) -> [1,2,4,5,7,9]
+ */
 const merge = (arr1, arr2) => {
     let newArr = [];
     let i = 0, j = 0;
@@ -46,4 +58,4 @@ const merge = (arr1, arr2) => {
     return newArr;
 }
 
-console.log(mergeSort([3, 7, 35, 53, 2, 3,12,3,24324,32,4,32]));
+console.log(mergeSort([3, 7, 35, 53, 2, 3, 12, 3, 24324, 32, 4, 32]));
